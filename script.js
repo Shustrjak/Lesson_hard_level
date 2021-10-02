f49600a486405c4890351567fc8afebeaf100f1b
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-02 20:09:13 +0300

    refactor script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let number = '266219';
-let iter;
 let pow = 1;
-for (iter = 0; iter < number.length; ++iter) {
+for (let iter = 0; iter < number.length; iter++) {
     pow *= number[iter];
 }
-pow **= 3;
+console.log(pow **= 3);
 console.log(pow.toString().substr(0, 2));
